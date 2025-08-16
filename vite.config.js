6e5b6e1490e39106fe716d1512c0b20a5f7d588c
dlidaniel <danielli4154@gmail.com>
2025-08-16 15:56:18 -0400

    Fix syntax

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb']
+  assetsInclude: ['**/*.glb'],
   base: process.env.VITE_BASE_PATH || '/3d-Galaxy-HackClub-Website',
 })
